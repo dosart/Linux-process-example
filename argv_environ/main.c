f08e7b679fbdc8af2147fd374005767f0dad7241
Denis <Dysprosium92@ya.ru>
2021-07-20 22:54:37 +0300

    Changed code style

--- argv_environ/main.c
+++ argv_environ/main.c
@@ -3,13 +3,14 @@
 
 extern char **environ;
 
-int main(int argc, char *argv[]) {
-  printf("The number of parameters passed to the program: %d\n", argc);
-  for (int i = 0; i < argc; ++i) {
-    printf("argv[%d] = %s \n", i, argv[i]);
-  }
-  for (int i = 0; environ[i]!=NULL; ++i) {
-    printf("environ[%s]\n", environ[i]);
-  }
-  return 0;
+int main(int argc, char *argv[])
+{
+	printf("The number of parameters passed to the program: %d\n", argc);
+	for (int i = 0; i < argc; ++i) {
+		printf("argv[%d] = %s \n", i, argv[i]);
+	}
+	for (int i = 0; environ[i] != NULL; ++i) {
+		printf("environ[%s]\n", environ[i]);
+	}
+	return 0;
 }
